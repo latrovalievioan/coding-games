3bd56d806bf10c6a29c36051557b2fa2c00334e5
Ioan Latrovaliev <latrovalievioan@gmail.com>
2024-06-13 19:16:40 +0300

    solution 3

---
+++ human-vs-zombies/solution3.ts
@@ -0,0 +1,142 @@
+/**
+ * Save humans, destroy zombies!
+ **/
+
+ type Entity = {
+  id?: number;
+  x: number;
+  y: number;
+};
+
+type HumanWithNearestZombie = {
+  human: Entity;
+  zombie: Entity;
+  zombieToHumanDistance: number;
+  meToHumanDistance: number;
+  meToZombieDistance: number;
+};
+
+const ZOMBIE_SPEED = 400;
+const ME_SPEED = 1000;
+const BULLET_DISTANCE = 2000;
+
+const getEntities = () => {
+  const entities: Entity[] = [];
+  const entityCount = parseInt(readline());
+  for (let i = 0; i < entityCount; i++) {
+    const inputs = readline().split(" ");
+    entities.push({
+      id: parseInt(inputs[0]),
+      x: parseInt(inputs[1]),
+      y: parseInt(inputs[2]),
+    });
+  }
+
+  return entities;
+};
+
+const getHumansWithNearestZombies = (
+  me: Entity,
+  humans: Entity[],
+  zombies: Entity[],
+) => {
+  const humansWithNearestZombies: HumanWithNearestZombie[] = [];
+  for (let i = 0; i < humans.length; i++) {
+    const human = humans[i];
+
+    const result: Partial<HumanWithNearestZombie> = {
+      human,
+    };
+
+    for (let j = 0; j < zombies.length; j++) {
+      const zombie = zombies[j];
+      const distance = Math.sqrt(
+        (human.x - zombie.x) ** 2 + (human.y - zombie.y) ** 2,
+      );
+
+      if (
+        !result.zombieToHumanDistance ||
+        distance < result.zombieToHumanDistance
+      ) {
+        const meToHumanDistance = Math.sqrt(
+          (human.x - me.x) ** 2 + (human.y - me.y) ** 2,
+        );
+
+        const meToZombieDistance = Math.sqrt(
+          (zombie.x - me.x) ** 2 + (zombie.y - me.y) ** 2,
+        );
+
+        result.zombieToHumanDistance = distance;
+        result.zombie = zombie;
+        result.meToHumanDistance = Math.floor(meToHumanDistance);
+        result.meToZombieDistance = Math.floor(meToZombieDistance);
+      }
+    }
+
+    humansWithNearestZombies.push(result as HumanWithNearestZombie);
+  }
+
+  return humansWithNearestZombies;
+};
+
+const calcDir = (
+  me: Entity,
+  humansWithNearestZombies: HumanWithNearestZombie[],
+) => {
+  //this funny optimization leads to 200 points more (not much but I should probably start thinking about combo optimization)
+  if(humansWithNearestZombies.length === 1) {
+    return `${humansWithNearestZombies[0].human.x} ${humansWithNearestZombies[0].human.y}`
+  }
+
+  const humanWithNearestZombie = humansWithNearestZombies.reduce(
+    (acc, curr) =>
+      curr.zombieToHumanDistance <
+      acc.zombieToHumanDistance
+        ? curr
+        : acc,
+    {
+      zombieToHumanDistance: Infinity,
+      meToHumanDistance: Infinity,
+        meToZombieDistance: Infinity,
+      human: {},
+      zombie: {},
+    } as HumanWithNearestZombie,
+  );
+
+  return `${humanWithNearestZombie.zombie.x} ${humanWithNearestZombie.zombie.y}`;
+};
+
+const filterUnsavableHumans = (
+  humansWithNearestZombies: HumanWithNearestZombie[],
+) => {
+  return humansWithNearestZombies.filter((humanWithNearestZombie) => {
+    const zombieToHumanTime =
+      humanWithNearestZombie.zombieToHumanDistance / ZOMBIE_SPEED;
+    //for some reson it filters with -1 better
+    const meToHumanTime = ((humanWithNearestZombie.meToHumanDistance - BULLET_DISTANCE) / ME_SPEED) - 1;
+
+    return zombieToHumanTime >= meToHumanTime;
+  });
+};
+
+// game loop
+while (true) {
+  const inputs = readline().split(" ");
+
+  const me: Entity = {
+    x: parseInt(inputs[0]),
+    y: parseInt(inputs[1]),
+  };
+
+  const humans = getEntities();
+
+  const zombies = getEntities();
+
+  const humansWithNearestZombies = getHumansWithNearestZombies(
+    me,
+    humans,
+    zombies,
+  );
+
+  console.log(calcDir(me, filterUnsavableHumans(humansWithNearestZombies)));
+}
